aea1009a84382b7d486da1d47f3a485b87772f4b
MatrixcsYounes <younesho9@outlook.com>
2023-01-20 20:42:20 +0100

    TP5 commit

--- src/net/mips/interpreter/Mnemonique.java
+++ src/net/mips/interpreter/Mnemonique.java
@@ -1,24 +1,5 @@
 package net.mips.interpreter;
 
 public enum Mnemonique {
-    INT,
-    ADD,
-    EQL,
-    SUB,
-    MUL,
-    DIV,
-    NEQ,
-    GTR,
-    LSS,
-    GEQ,
-    LEQ,
-    PRN,
-    INN,
-    LDI,
-    LDA,
-    LDV,
-    STO,
-    BRN,
-    BZR,
-    HLT;
+    INT,ADD,EQL,SUB,MUL,DIV,NEQ,GTR,LSS,GEQ,LEQ,PRN,INN,LDI,LDA,BZE,LDV,STO,BRN,HLT,NUM,EOF;
 }
